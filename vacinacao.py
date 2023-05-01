ba60a0ce4dc021cf424248a8291fcc85288b012f
Augusto <129184930+Augusto240@users.noreply.github.com>
2023-04-30 21:01:43 -0300

    Add files via upload

---
+++ vacinacao.py
@@ -0,0 +1,13 @@
+#programa mostra se a cacina contra a gripe estará disponível ou não, baseado na idade da criança
+
+idade = eval(input('Informe a idade da criança: \n'))
+if idade < 5:
+    print('A criança deve ser vacinada contra a gripe.')
+    print('Procure o posto de saúde mais próximo.')
+elif idade == 5:
+    print('A vacina estará disponível em breve.')
+    print('Aguarde as próximas informações.')
+else:
+    print('A vacinação só ocorrerá daqui a 3 meses.')
+    print('Informe-se novamente neste prazo.')
+print('Cuide da saúde sempre. Até a próxima.')
